92eba383cefdc5196a3da38883ad7e31f0f829c9
Adrià Hernández Morell <adriahernandez.2001@gmail.com>
2021-06-22 22:39:07 +0200

    Voting_Platform-First_Sketch

---
+++ Voting Platform/app_voting_platform.js
@@ -0,0 +1,19 @@
+// const side_bar = document.querySelector('.side_bar');
+// const side_bar_list = document.querySelector('.side_bar_list');
+// const side_bar_pancake = document.querySelector('.social_media-img_pancake');
+// const side_bar_twitch = document.querySelector('.social_media-img_twitch');
+// const side_bar_youtube = document.querySelector('.social_media-img_youtube');
+// const side_bar_twitter = document.querySelector('.social_media-img_twitter');
+// const side_bar_telegram = document.querySelector('.social_media-img_telegram');
+
+
+// document.querySelector('.toggle').onclick = function(){
+//   this.classList.toggle('active');
+//   side_bar.classList.toggle('active');
+//   side_bar_list.classList.toggle('active');
+//   side_bar_pancake.classList.toggle('active');
+//   side_bar_twitch.classList.toggle('active');
+//   side_bar_youtube.classList.toggle('active');
+//   side_bar_twitter.classList.toggle('active');
+//   side_bar_telegram.classList.toggle('active');
+// }
